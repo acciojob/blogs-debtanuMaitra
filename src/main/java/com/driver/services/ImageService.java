6d18a6d6d95c5478d79b4ceecad5e4d2f9017e55
Debtanu Maitra <maitradebtanu@gmail.com>
2023-02-18 19:41:38 +0530

    Solution_1

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,25 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
-
+        Blog blog = blogRepository2.findById(blogId).get();
+        Image image = new Image(description, dimensions, blog);
+        blog.getImageList().add(image);
+        blogRepository2.save(blog);
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
-
+        String[] screenSizeArr = screenDimensions.split("X");
+        Image image = imageRepository2.findById(id).get();
+        String imageDimension = image.getDimensions();
+        String[] imageSizeArr = imageDimension.split("X");
+        int screenArea = Integer.parseInt(screenSizeArr[0]) * Integer.parseInt(screenSizeArr[1]);
+        int imageArea = Integer.parseInt(imageSizeArr[0]) * Integer.parseInt(imageSizeArr[1]);
+        return screenArea / imageArea;
     }
 }
